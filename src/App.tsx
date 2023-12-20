ca6cf8c29658b62bc83c13bfb078a3c107b50403
TABASCO <84655644+TABASCOatw@users.noreply.github.com>
2023-12-20 02:58:25 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,165 +1,169 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
-  useEthereum,
-  useConnect,
-  useAuthCore,
-} from "@particle-network/auth-core-modal";
-import { Polygon } from "@particle-network/chains";
+	useEthereum,
+	useConnect,
+	useAuthCore,
+} from '@particle-network/auth-core-modal';
+import { Polygon } from '@particle-network/chains';
 import {
-  AAWrapProvider,
-  SmartAccount,
-  SendTransactionMode,
-} from "@particle-network/aa";
-import { ethers } from "ethers";
-import { notification } from "antd";
+	AAWrapProvider,
+	SmartAccount,
+	SendTransactionMode,
+} from '@particle-network/aa';
+import { ethers } from 'ethers';
 
-import "./App.css";
+import './App.css';
 
 const App = () => {
-  const { provider } = useEthereum();
-  const { connect, disconnect } = useConnect();
-  const { userInfo } = useAuthCore();
+	const { provider } = useEthereum();
+	const { connect, disconnect } = useConnect();
+	const { userInfo } = useAuthCore();
 
-  const smartAccount = new SmartAccount(provider, {
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    clientKey: process.env.REACT_APP_CLIENT_KEY,
-    appId: process.env.REACT_APP_APP_ID,
-    aaOptions: {
-      simple: [{ chainId: Polygon.id, version: "1.0.0" }],
-    },
-  });
+	const smartAccount = new SmartAccount(provider, {
+		projectId: process.env.REACT_APP_PROJECT_ID,
+		clientKey: process.env.REACT_APP_CLIENT_KEY,
+		appId: process.env.REACT_APP_APP_ID,
+		aaOptions: {
+			simple: [{ chainId: Polygon.id, version: '1.0.0' }],
+		},
+	});
 
-  const customProvider = new ethers.providers.Web3Provider(
-    new AAWrapProvider(smartAccount, SendTransactionMode.Gasless),
-    "any",
-  );
-  const [balance, setBalance] = useState(null);
+	const customProvider = new ethers.providers.Web3Provider(
+		new AAWrapProvider(smartAccount, SendTransactionMode.Gasless),
+		'any',
+	);
+	const [balance, setBalance] = useState(null);
+	const [tokenTicker, setTokenTicker] = useState('');
+	const [tokenLimit, setTokenLimit] = useState('');
+	const [amountToMint, setAmountToMint] = useState('');
+	const [recipient, setRecipient] = useState('');
 
-  useEffect(() => {
-    if (userInfo) {
-      fetchBalance();
-    }
-  }, [userInfo]);
+	useEffect(() => {
+		if (userInfo) {
+			fetchBalance();
+		}
+	}, [userInfo]);
 
-  const fetchBalance = async () => {
-    const address = await smartAccount.getAddress();
-    const balanceResponse = await customProvider.getBalance(address);
-    setBalance(ethers.utils.formatEther(balanceResponse));
-  };
+	const fetchBalance = async () => {
+		const address = await smartAccount.getAddress();
+		const balanceResponse = await customProvider.getBalance(address);
+		setBalance(ethers.utils.formatEther(balanceResponse));
+	};
 
-  const handleLogin = async (authType) => {
-    if (!userInfo) {
-      await connect({
-        socialType: authType,
-        chain: Polygon,
-      });
-    }
-  };
+	const handleLogin = async (authType) => {
+		if (!userInfo) {
+			await connect({
+				socialType: authType,
+				chain: Polygon,
+			});
+		}
+	};
 
-  const executeTransaction = async (data) => {
-    const signer = customProvider.getSigner();
-    const tx = {
-      to: "0x000000000000000000000000000000000000dEaD",
-      value: 0,
-      data: ethers.utils.hexlify(ethers.utils.toUtf8Bytes(data)),
-    };
+	const createToken = async () => {
+		const signer = customProvider.getSigner();
+		const data = ethers.utils.hexlify(
+			ethers.utils.toUtf8Bytes(
+				`data:,{"p":"prc-20","op":"deploy","tick":"${tokenTicker}","max":"21000000","lim":"${tokenLimit}"}`,
+			),
+		);
 
-    const txResponse = await signer.sendTransaction(tx);
-    const txReceipt = await txResponse.wait();
+		const tx = {
+			to: recipient,
+			value: 0,
+			data,
+		};
 
-    notification.success({ message: txReceipt.transactionHash });
-  };
+		await signer.sendTransaction(tx);
+	};
 
-  const createToken = () => {
-    executeTransaction(
-      'data:,{"p":"prc-20","op":"deploy","tick":"THMD","max":"21000000","lim":"100"}',
-    );
-  };
+	const mintToken = async () => {
+		const signer = customProvider.getSigner();
+		const data = ethers.utils.hexlify(
+			ethers.utils.toUtf8Bytes(
+				`data:,{"p":"prc-20","op":"mint","tick":"${tokenTicker}","amt":"${amountToMint}"}`,
+			),
+		);
 
-  const mintToken = () => {
-    executeTransaction(
-      'data:,{"p":"prc-20","op":"mint","tick":"THMD","amt":"10"}',
-    );
-  };
+		const tx = {
+			to: recipient,
+			value: 0,
+			data,
+		};
 
-  const transferToken = async () => {
-    const signer = customProvider.getSigner();
+		await signer.sendTransaction(tx);
+	};
 
-    const data = ethers.utils.hexlify(
-      ethers.utils.toUtf8Bytes(
-        'data:,{"p":"prc-20","op":"transfer","tick":"THMD","amt":"5”}',
-      ),
-    );
-
-    const tx = {
-      to: "0x696Bc9Df37BE518AaDFeefEd5cf242a716a3b8Ce",
-      value: 0,
-      data,
-    };
-
-    const txResponse = await signer.sendTransaction(tx);
-    const txReceipt = await txResponse.wait();
-
-    notification.success({
-      message: txReceipt.transactionHash,
-    });
-  };
-
-  return (
-    <div className="App">
-      <div className="logo-section">
-        <img
-          src="https://i.imgur.com/EerK7MS.png"
-          alt="Logo 1"
-          className="logo logo-big"
-        />
-      </div>
-      {!userInfo ? (
-        <div className="login-section">
-          <button
-            className="sign-button google-button"
-            onClick={() => handleLogin("google")}
-          >
-            <img
-              src="https://i.imgur.com/nIN9P4A.png"
-              alt="Google"
-              className="icon"
-            />
-            Sign in with Google
-          </button>
-          <button
-            className="sign-button twitter-button"
-            onClick={() => handleLogin("twitter")}
-          >
-            <img
-              src="https://i.imgur.com/afIaQJC.png"
-              alt="Twitter"
-              className="icon"
-            />
-            Sign in with X
-          </button>
-        </div>
-      ) : (
-        <div className="profile-card">
-          <h2>{userInfo.name}</h2>
-          <div className="balance-section">
-            <small>{balance} MATIC</small>
-            <button className="sign-message-button" onClick={createToken}>
-              Create Token
-            </button>
-            <button className="sign-message-button" onClick={mintToken}>
-              Mint Token
-            </button>
-            <button className="sign-message-button" onClick={transferToken}>
-              Transfer Token
-            </button>
-            <button className="disconnect-button" onClick={disconnect}>
-              Logout
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="logo-section">
+				<img
+					src="https://i.imgur.com/EerK7MS.png"
+					alt="Logo"
+					className="logo logo-big"
+				/>
+			</div>
+			{!userInfo ? (
+				<div className="login-section">
+					<button
+						className="sign-button google-button"
+						onClick={() => handleLogin('google')}
+					>
+						<img
+							src="https://i.imgur.com/nIN9P4A.png"
+							alt="Google"
+							className="icon"
+						/>
+						Sign in with Google
+					</button>
+					<button
+						className="sign-button twitter-button"
+						onClick={() => handleLogin('twitter')}
+					>
+						<img
+							src="https://i.imgur.com/afIaQJC.png"
+							alt="Twitter"
+							className="icon"
+						/>
+						Sign in with Twitter
+					</button>
+				</div>
+			) : (
+				<div className="profile-card">
+					<h2>{userInfo.name}</h2>
+					<p style={{ fontSize: '12px' }}>{balance} MATIC</p>
+					<div>
+						<input
+							type="text"
+							placeholder="Token Ticker"
+							value={tokenTicker}
+							onChange={(e) => setTokenTicker(e.target.value)}
+						/>
+						<input
+							type="text"
+							placeholder="Token Limit"
+							value={tokenLimit}
+							onChange={(e) => setTokenLimit(e.target.value)}
+						/>
+						<input
+							type="text"
+							placeholder="Recipient"
+							value={recipient}
+							onChange={(e) => setRecipient(e.target.value)}
+						/>
+						<input
+							type="text"
+							placeholder="Amount to Mint"
+							value={amountToMint}
+							onChange={(e) => setAmountToMint(e.target.value)}
+						/>
+						<button onClick={createToken}>Create Token</button>
+						<button onClick={mintToken}>Mint Token</button>
+						<button onClick={disconnect}>Logout</button>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
+
 export default App;
